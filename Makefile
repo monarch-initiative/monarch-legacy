b312f697ee8241a8264d558478ba85cbed52cddf
Chris Mungall <cjm@berkeleybop.org>
2013-09-09 18:56:14 -0700

    command line tools

--- Makefile
+++ Makefile
@@ -3,6 +3,11 @@
 docs:
 	naturaldocs --rebuild-output --input lib/monarch --project lib/.naturaldocs_project/ --output html docs/
 
+deploy: origin-push heroku-deploy
+
+origin-push:
+	git push origin master
+
 ## http://secret-harbor-1370.herokuapp.com/
 heroku-create:
 	heroku create --stack cedar --buildpack https://github.com/cmungall/heroku-buildpack-ringojs-jdk7.git --remote monarch-heroku
--- bin/fetch-disease-info.js
+++ bin/fetch-disease-info.js
@@ -1,16 +1,19 @@
 load('lib/monarch/api.js');
 var Parser = require('ringo/args').Parser;
 var system = require('system');
-
+var httpclient = require('ringo/httpclient');
 
 function main(args) {
+    var script = args.shift();
     var parser = new Parser(system.args);
 
+    parser.addOption("q","query","QueryTerm", "ID or name for a disease");
+    parser.addOption("x", "extract", "DocumentPart", "E.g. phenotype_associations");
+    parser.addOption('h', 'help', null, 'Display help');
+    parser.addOption('c', 'context', null, 'Add JSON-LD context');
+    //parser.addOption('', 'no-context', null, 'Hide JSON-LD context');
+    //parser.addOption('p', 'prettyprint', null, 'Pretty print JSON');
 
-    parser.addOption("i","identifer","ID", "for a disease");
-    parser.addOption("x");
-
-    print(JSON.stringify(args));
     var options = parser.parse(args);
 
     if (options.help) {
@@ -18,13 +21,29 @@ function main(args) {
 	system.exit('-1');
     }
 
-    var id = options.id;
-    print(JSON.stringify(options));
-    print(id);
+    print("Q="+options.query);
 
     var engine = new bbop.monarch.Engine();
-    var info = engine.fetchDiseaseInfo(id); 
-    print(JSON.stringify(info));
+    var info = engine.fetchDiseaseInfo(options.query); 
+
+    if (options.extract != null) {
+        info = info[options.extract];
+    }
+
+    if (options.context) {
+        engine.addJsonLdContext(info);
+    }
+    else {
+        info['@context'] = null;
+    }
+
+    var out = JSON.stringify(info, null, " ");
+
+    //if (options.prettyprint) {
+        //out = httpclient.post("http://jsonprettyprint.com/json-pretty-printer.php", {json_data:out}).content;
+//}
+
+    print(out);
 }
 
 // call the main method; ringo specific
