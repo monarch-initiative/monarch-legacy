a6a2af36dfb2c8fe7e41f2d5836eba95f44dcbe5
Chris Mungall <cjm@berkeleybop.org>
2013-11-07 14:36:14 -0800

    for tartini

--- start-server.sh
+++ start-server.sh
@@ -1,1 +1,10 @@
-./ringojs/bin/ringo lib/monarch/web/webapp_launcher.js --port 8282
+#!/bin/sh
+PATH_TO_ME=`which $0`
+cd `dirname $PATH_TO_ME`
+if [ $PORT ]
+  then
+   MARGS="--port $PORT"
+  else
+   MARGS="--port 8282"
+fi
+./ringojs/bin/ringo lib/monarch/web/webapp_launcher.js $MARGS
