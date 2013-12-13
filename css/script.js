dd2b359dd7b55b7ddc8b6ea4036a871003427d3f
Sarah Kim <jiyunie13@gmail.com>
2013-12-13 12:53:24 -0800

    Sidebar now changes tab color as well

--- css/script.js
+++ css/script.js
@@ -14,23 +14,32 @@ $(document).ready(function(){
     $('.special').click(function() {
         var panel_id = $(this).attr("id");
         $('.category').hide();
+        $('.tab').css({'color': 'grey'});
         var panel;
         if (panel_id == "over") {
             panel = '#overview';
+            tab = '.overtab';
         } else if (panel_id == "ont") {
             panel = '#ontology';
+            tab = '.onttab';
         } else if (panel_id == "phen") {
             panel = '#phenotype';
+            tab = '.phentab';
         } else if (panel_id == "gen") {
             panel = '#gene';
+            tab = '.gentab';
         } else if (panel_id == "alle") {
             panel = '#allele';
+            tab = '.alletab';
         } else if (panel_id == "mod") {
             panel = '#model';
+            tab = '.modtab';
         } else {
             panel = '#sim';
+            tab = '.simtab';
         }
         $(panel).show(400);
+        $(tab).css({'color': 'white'});
     });
 
 });
