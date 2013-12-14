d0da82bdc84a6f30f7d993b3def5619632a40c10
Sarah Kim <jiyunie13@gmail.com>
2013-12-13 21:10:40 -0800

    Undid changes to start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,1 +1,12 @@
-./ringojs/bin/ringo -J-Xmx2G lib/monarch/web/webapp_launcher.js --port 8282
+#!/bin/sh
+PATH_TO_ME=`which $0`
+cd `dirname $PATH_TO_ME`
+if [ $PORT ]
+  then
+   MARGS="--port $PORT"
+  else
+   echo using default port
+   MARGS="--port 8080"
+fi
+echo starting server
+./ringojs/bin/ringo lib/monarch/web/webapp_launcher.js $MARGS
