759e35be0e5bc3bc8d85d408ee7d420cdbaced5d
Sarah Kim <jiyunie13@gmail.com>
2014-05-20 15:56:21 -0700

    Removed legacy code
    I forgot to remove the code related to the sidebar (which was deprecated a while back).

--- js/tabs.js
+++ js/tabs.js
@@ -18,42 +18,4 @@ $(document).ready(function(){
         $(panel_id).show();
     });
 
-    $('.special').click(function(event) {
-        var panel_id = $(this).attr("id");
-        $('.category').hide();
-        $('.contenttab').css({'color': 'white', 'background-color': '#999', 'border-bottom': '1px solid black'});
-        var panel;
-        if (panel_id == "phen") {
-            panel = '#phenotypes';
-        } else if (panel_id == "ont") {
-            panel = '#ontology';
-        } else if (panel_id == "gen") {
-            panel = '#genes';
-        } else if (panel_id == "alle") {
-            panel = '#alleles';
-        } else if (panel_id == "mod") {
-            panel = '#model';
-        } else if (panel_id == "simi") {
-            panel = '#similarity';
-        } else if (panel_id == "path") {
-            panel = '#pathways';
-        } else if (panel_id == "over") {
-            panel = '#overview';
-        } else if (panel_id == "dis") {
-            panel = '#disease';
-        } else if (panel_id == "geno") {
-            panel = '#genotypes';
-        } else if (panel_id == "que") {
-            panel = '#query';
-        } else if (panel_id == "res") {
-            panel = '#result';
-        } else if (panel_id == "gri") {
-            panel = '#grid';
-        }
-        contenttab = '.' + panel_id + 'tab';
-        event.preventDefault();
-        $(panel).show();
-        $(contenttab).css({'color': 'black', 'background-color': 'white', 'border-bottom': '1px solid white'});
-    });
-
 });
