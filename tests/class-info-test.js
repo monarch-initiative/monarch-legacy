24826cb4be1097bada778fb59d1ce49c7de7b01e
Chris Mungall <cjm@berkeleybop.org>
2014-06-09 10:50:17 -0700

    more stringent tests for class-info, ensure we have def, label, id etc. Issue #294

--- tests/class-info-test.js
+++ tests/class-info-test.js
@@ -5,6 +5,7 @@ var engine;
 var classIds =
     [
         "DOID:14330", // PD
+        "DOID_12358", // patulous eustachian tube
     ];
 
 exports.testClassInfo = function() {
@@ -12,6 +13,10 @@ exports.testClassInfo = function() {
         function(id) {
             var resObj = engine.fetchClassInfo(id);
             console.log(JSON.stringify(resObj,' ',1));
+            assert.isTrue(resObj.definition.length > 0);
+            assert.isTrue(resObj.id.length > 0);
+            assert.isTrue(resObj.label.length > 0);
+            assert.isTrue(resObj.category == 'disease');
             // todo - check json
             //assert.isTrue(resObj.resultCount > 100);
             //assert.isTrue(resObj.results.length > 100);
