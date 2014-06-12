e0111de5a9fcdd2205b8f7f6de1393d140fbbd63
kshefchek <kshefchek@gmail.com>
2014-06-12 00:29:34 -0400

    made graph horizontal, sorted by mouse counts

--- js/datagraph.js
+++ js/datagraph.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+/*
 var margin = {top: 30, right: 60, bottom: 200, left: 180},
 width = 1300 - margin.left - margin.right,
 height = 600 - margin.top - margin.bottom;
@@ -105,4 +105,113 @@ d3.json("/labs/datagraph.json", function(error, json) {
        .style("text-anchor", "end")
        .text(function(d) { return d; });   
     });
+    */
+	var margin = {top: 30, right: 80, bottom: 200, left: 320},
+	width = 1000 - margin.left - margin.right,
+	height = 900 - margin.top - margin.bottom;
+
+	var y0 = d3.scale.ordinal()
+	    .rangeRoundBands([0,height], .1);
+
+	var y1 = d3.scale.ordinal();
+
+	var x = d3.scale.linear()
+	    .range([0, width]);
+
+	var color = d3.scale.ordinal()
+	    .range(["#8a89a6","#98abc5", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom")
+	    .tickFormat(d3.format(".2s"));
+
+	var yAxis = d3.svg.axis()
+	    .scale(y0)
+	    .orient("left");
+	    //.tickFormat(d3.format(".2s"));
+
+	var svg = d3.select("#graph").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	    .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	d3.json("/labs/datagraph.json", function(error, json) {
+
+	    var groups = json.groups;
+	    var data = json.dataGraph;
+
+	    y0.domain(data.map(function(d) { return d.phenotype; }));
+	    y1.domain(groups).rangeRoundBands([0, y0.rangeBand()]);
+	    x.domain([0, d3.max(data, function(d) { 
+	    	return d3.max(d.counts, function(d) { return d.value; }); })]);
+	    
+	    svg.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(xAxis)
+	    .append("text")
+	    .attr("transform", "rotate(0)")
+	    .attr("y", 6)
+	    .attr("dx", "25em")
+	    .attr("dy", "3em")
+	    .style("text-anchor", "end")
+	    .text("Phenotype Counts");
+
+	    svg.append("g")
+	    .attr("class", "y axis")
+	    .call(yAxis)
+	    .selectAll("text")  
+	    .style("text-anchor", "end")
+	    .attr("dx", "-.5em")
+	    .attr("dy", ".15em")
+	    .attr("transform", function(d) {
+	        return "rotate(0)" 
+	        });
+
+	    var phenotype = svg.selectAll(".phenotype")
+	        .data(data)
+	        .enter().append("svg:a")
+	        .attr("class", "bar")
+	        .attr("xlink:href", function(d) { return "/Phenotype/"+ d.id; })
+	        .attr("transform", function(d) { return "translate(0," + y0(d.phenotype) + ")"; });
+
+	    phenotype.selectAll("rect")
+	       .data(function(d) { return d.counts; })
+	       .enter().append("rect")
+	       .attr("height", y1.rangeBand())
+	       .attr("y", function(d) { return y1(d.value); })
+	       .attr("x", 0)
+	       .attr("width", function(d) { return x(d.value); })
+	       .on("mouseover", function(){
+	           d3.select(this)
+	           .style("fill", "#71B291");
+	        })
+	       .on("mouseout", function(){
+	           d3.select(this)
+	           .style("fill", function(d) { return color(d.name); });
+	        })
+	       .style("fill", function(d) { return color(d.name); });
+	    
+	    var legend = svg.selectAll(".legend")
+	       .data(groups.slice())
+	       .enter().append("g")
+	       .attr("class", "legend")
+	       .attr("transform", function(d, i) { return "translate(0," + i * 25 + ")"; });
+
+	    legend.append("rect")
+	       .attr("x", width+55)
+	       .attr("y", 4)
+	       .attr("width", 18)
+	       .attr("height", 18)
+	       .style("fill", color);
+
+	    legend.append("text")
+	       .attr("x", width+50)
+	       .attr("y", 12)
+	       .attr("dy", ".35em")
+	       .style("text-anchor", "end")
+	       .text(function(d) { return d; });   
+	});
 });
