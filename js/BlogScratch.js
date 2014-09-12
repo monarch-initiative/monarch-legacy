e941bdaad82518ccbf886267efedc41df8f3e2e6
SC <github.com@genkisugi.net>
2014-09-11 17:53:25 -0700

    work on #518; another redo of the landing page, with an attempt to fold in comments from the meeting this morning; most sub-tasks completed

--- js/BlogScratch.js
+++ js/BlogScratch.js
@@ -4,50 +4,59 @@
 
 jQuery(document).ready(function(){
 
-    ///
-    /// Do a demo D3 chart from docs.
-    ///
+    // ///
+    // /// Do a demo D3 chart from docs.
+    // ///
 
-    var data = [4, 8, 15, 16, 23, 35, 42, 3, 3, 6, 9, 11, 18, 26];
+    // var data = [4, 8, 15, 16, 23, 35, 42, 3, 3, 6, 9, 11, 18, 26];
 
-    var width = 420;
-    var barHeight = 20;
+    // var width = 420;
+    // var barHeight = 20;
 
-    var x = d3.scale.linear()
-	.domain([0, d3.max(data)])
-	.range([0, width]);
+    // var x = d3.scale.linear()
+    // 	.domain([0, d3.max(data)])
+    // 	.range([0, width]);
     
-    var chart = d3.select("#d3-chart")
-	.attr("width", width)
-	.attr("height", barHeight * data.length);
+    // var chart = d3.select("#d3-chart")
+    // 	.attr("width", width)
+    // 	.attr("height", barHeight * data.length);
     
-    var bar = chart.selectAll("g")
-	.data(data)
-	.enter().append("g")
-	.attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+    // var bar = chart.selectAll("g")
+    // 	.data(data)
+    // 	.enter().append("g")
+    // 	.attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
     
-    bar.append("rect")
-	.attr("width", x)
-	.attr("height", barHeight - 1);
+    // bar.append("rect")
+    // 	.attr("width", x)
+    // 	.attr("height", barHeight - 1);
     
-    bar.append("text")
-	.attr("x", function(d) { return x(d) - 3; })
-	.attr("y", barHeight / 2)
-	.attr("dy", ".35em")
-	.text(function(d) { return d; });
+    // bar.append("text")
+    // 	.attr("x", function(d) { return x(d) - 3; })
+    // 	.attr("y", barHeight / 2)
+    // 	.attr("dy", ".35em")
+    // 	.text(function(d) { return d; });
 
     ///
     /// Do a demo ticker.
     ///
 
-    var ticker_cache = [
-	'Cras justo odio',
-	'Dapibus ac facilisis in',
-	'Morbi leo risus',
-	'Porta ac consectetur ac',
-	    'Vestibulum at eros',
-    ];
+    // var ticker_cache = [
+    // 	'Cras justo odio',
+    // 	'Dapibus ac facilisis in',
+    // 	'Morbi leo risus',
+    // 	'Porta ac consectetur ac',
+    // 	'Vestibulum at eros',
+    // 	'Cras justo odio 2',
+    // 	'Dapibus ac facilisis in 2',
+    // 	'Morbi leo risus 2',
+    // 	'Porta ac consectetur ac 2',
+    // 	'Vestibulum at eros 2'
+    // ];
     
+    // // Add the above to the doc at point.
+
+    // The cycler.
+    var ticker_cache = global_data_ticker;
     function _cycle_ticker(){
 
 	// Rotate top item to end.
@@ -60,7 +69,8 @@ jQuery(document).ready(function(){
 	    jQuery('#ticker-demo li').first().remove();
 	}); 
 	// Add and slide in. Plus restart wait (arbitrary--could be above).
-	var new_elt = '<li class="list-group-item">' + top +'</li>';
+	var new_elt = '<li class="list-group-item data-ticker-line">' + top +'</li>';
+	// var new_elt = '<tr><td>' + top + '</td></tr>';
 	jQuery(new_elt).hide().appendTo('#ticker-demo').slideDown(500,function(){
 	    // When done, start countdown to new cycle.
 	    window.setTimeout(_cycle_ticker, 3500);
