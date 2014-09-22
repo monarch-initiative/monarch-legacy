597c44e1c9ec5f2682b05d47fcf2d71ee914093b
Harry Hochheiser <harryh@pitt.edu>
2014-09-21 22:15:05 -0400

    moved phenogrid widget to separate repository

--- install.sh
+++ install.sh
@@ -4,3 +4,11 @@ wget http://ringojs.org/downloads/$V.tar.gz
 tar -zxvf $V.tar.gz
 ln -s $V ringojs
 ./ringojs/bin/ringo-admin install ringo/stick
+
+CWD=`pwd`
+# install phenogrid
+if [ ! -d ./widgets/phenogrid ]; then
+    cd ./widgets
+    git clone https://github.com/monarch-initiative/phenogrid.git
+    cd $CWD
+fi
--- install_mac.sh
+++ install_mac.sh
@@ -4,3 +4,8 @@ curl -O http://ringojs.org/downloads/$V.tar.gz
 tar -zxvf $V.tar.gz
 ln -s $V ringojs
 ./ringojs/bin/ringo-admin install ringo/stick
+if [ ! -d ./widgets/phenogrid ]; then
+    cd ./widgets
+    git clone https://github.com/monarch-initiative/phenogrid.git
+    cd $CWD
+fi
