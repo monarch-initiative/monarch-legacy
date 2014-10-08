71517d51133455f80170407430af72d5f118e94e
Harry Hochheiser <harryh@pitt.edu>
2014-10-07 21:30:17 -0400

    working toward genotype page phenogrid

--- js/diseasepage.js
+++ js/diseasepage.js
@@ -3,12 +3,16 @@ $(function () {
     var disease_id = this.location.pathname;
     var slash_idx = disease_id.indexOf('/');
     disease_id = disease_id.substring(slash_idx+1);
-    var phenotype_list = []; 
+    var phenotype_list = [];
+    console.log("disease page ...id is..."+disease_id);
+    var url = '/' + disease_id + '/phenotype_associations.json'; 
+    console.log("url is "+ url);
+
     jQuery.ajax({ 
 	url : '/' + disease_id + '/phenotype_associations.json', 
 	async : false, 
 	dataType : 'json', 
-	success : function(data) { 
+	success : function(data) {
 	    for (var idx=0;idx<data.phenotype_associations.length;idx++) { 
 		phenotype_list.push({ "id" : data.phenotype_associations[idx].phenotype.id, "label" : data.phenotype_associations[idx].phenotype.label,
 		         "observed" : "positive"}); 
--- js/genotypepage.js
+++ js/genotypepage.js
@@ -3,13 +3,15 @@ $(function () {
     var id = this.location.pathname;
     var slash_idx = id.indexOf('/');
     id = id.substring(slash_idx+1);
-    var phenotype_list = []; 
+    var phenotype_list = [];
+    console.log("genotype page ...id is..."+id);
+    
     jQuery.ajax({ 
 		url : '/' + id + '/phenotype_associations.json', 
 		async : false, 
 		dataType : 'json', 
-		success : function(data) {
-			for (var idx=0;idx<data.phenotype_associations.length;idx++) {
+	success : function(data) {
+		    for (var idx=0;idx<data.phenotype_associations.length;idx++) {
 				var assoc = data.phenotype_associations[idx];
 				if (typeof assoc.has_phenotype != 'undefined' && typeof assoc.has_phenotype.type != 'undefined') {
 					pheno = assoc.has_phenotype.type;
@@ -20,5 +22,5 @@ $(function () {
 		}
     });
 
-	$("#phen_vis").modeltype({phenotypeData: phenotype_list});
+	$("#phen_vis").phenogrid({phenotypeData: phenotype_list});
 });        
