95cde0a1083fc52b9023612c9c68f17d6fb55d11
Harry Hochheiser <harryh@pitt.edu>
2014-10-15 07:19:16 -0400

    minor fixes

--- js/diseasepage.js
+++ js/diseasepage.js
@@ -1,12 +1,10 @@
 
 $(function () { 
-/*    var disease_id = this.location.pathname;
+    var disease_id = this.location.pathname;
     var slash_idx = disease_id.indexOf('/');
-    disease_id = disease_id.substring(slash_idx+1);*/
+    disease_id = disease_id.substring(slash_idx+1);
     var phenotype_list = [];
-    /* console.log("disease page ...id is..."+disease_id);*/
     var url = '/' + disease_id + '/phenotype_associations.json'; 
-    /*console.log("url is "+ url);*/
 
     jQuery.ajax({ 
 	url : '/' + disease_id + '/phenotype_list.json', 
