cc134a76692514ac28a404fe1a21b8dbc9077fba
Christopher Condit <condit@sdsc.edu>
2015-04-28 15:21:27 -0700

    Adjusts UUID field reference #678

--- scripts/remove-schema-cruft.pl
+++ scripts/remove-schema-cruft.pl
@@ -5,5 +5,8 @@ while(<>){
   if (/\<\/types\>/) { # Add the UUID type so Solr can autogenerate IDs
     print "    <fieldType name=\"uuid\" class=\"solr.UUIDField\" indexed=\"true\" />\n";
   } 
+  if (/type=\"uuid\"/) {
+    $_ = "    <field name=\"id\" type=\"uuid\" indexed=\"true\" stored=\"true\" default=\"NEW\"/>\n";
+  } 
   print $_ if $b;
 }
