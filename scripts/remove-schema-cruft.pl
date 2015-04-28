b4d2092a502162ace28c46aceaffc9fad804c0ad
Christopher Condit <condit@sdsc.edu>
2015-04-28 14:43:49 -0700

    Adds UUID field type so Solr IDs can auto generate

--- scripts/remove-schema-cruft.pl
+++ scripts/remove-schema-cruft.pl
@@ -2,5 +2,8 @@
 my $b = 0;
 while(<>){
   $b = 1 if /\<\?xml/;
+  if (/\<\/types\>/) { # Add the UUID type so Solr can autogenerate IDs
+    print "    <fieldType name=\"uuid\" class=\"solr.UUIDField\" indexed=\"true\" />\n";
+  } 
   print $_ if $b;
 }
