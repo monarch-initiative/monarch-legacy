5e07ee280d8986616af5d3283e727758171ff1cf
kshefchek <kshefchek@gmail.com>
2015-05-27 12:56:23 -0700

    add scripts to bundle monarch libs (just handler and linker for now)

---
+++ js/version.js
@@ -0,0 +1,27 @@
+/* 
+ * Package: version.js
+ * 
+ * Namespace: monarch.version
+ * 
+ * This package was automatically generated during the build process
+ * and contains its version information--this is the release of the
+ * API that you have.
+ */
+
+if ( typeof monarch == "undefined" ){ var monarch = {}; }
+if ( typeof monarch.version == "undefined" ){ monarch.version = {}; }
+
+/*
+ * Variable: revision
+ *
+ * Partial version for this library; revision (major/minor version numbers)
+ * information.
+ */
+monarch.version.revision = "0.1.1";
+
+/*
+ * Variable: release
+ *
+ * Partial version for this library: release (date-like) information.
+ */
+monarch.version.release = "20150527";
