b589b0fb7f010070f51a6182bb421d9725ae178e
kshefchek <kshefchek@gmail.com>
2015-06-22 14:00:27 -0700

    add rest of yaml files, script to create yaml files

---
+++ scripts/make-all-tab-yamls.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Phenotype
+./make-tab-yaml.pl Phenotype Disease > ../conf/golr-views/single-tab/phenotype-disease.yaml
+./make-tab-yaml.pl Phenotype Model > ../conf/golr-views/single-tab/phenotype-model.yaml
+./make-tab-yaml.pl Phenotype Variant > ../conf/golr-views/single-tab/phenotype-variant.yaml
+./make-tab-yaml.pl Phenotype Pathway > ../conf/golr-views/single-tab/phenotype-pathway.yaml
+./make-tab-yaml.pl Phenotype Gene > ../conf/golr-views/single-tab/phenotype-gene.yaml
+
+# Disease
+./make-tab-yaml.pl Disease Phenotype > ../conf/golr-views/single-tab/disease-phenotype.yaml
+./make-tab-yaml.pl Disease Gene > ../conf/golr-views/single-tab/disease-gene.yaml
+./make-tab-yaml.pl Disease Model > ../conf/golr-views/single-tab/disease-model.yaml
+./make-tab-yaml.pl Disease Variant > ../conf/golr-views/single-tab/disease-variant.yaml
+./make-tab-yaml.pl Disease Pathway > ../conf/golr-views/single-tab/disease-pathway.yaml
+
+# Variant
+./make-tab-yaml.pl Variant Phenotype > ../conf/golr-views/single-tab/variant-phenotype.yaml
+./make-tab-yaml.pl Variant Disease > ../conf/golr-views/single-tab/variant-disease.yaml
+./make-tab-yaml.pl Variant Model > ../conf/golr-views/single-tab/variant-model.yaml
+./make-tab-yaml.pl Variant Pathway > ../conf/golr-views/single-tab/variant-pathway.yaml
+
+# Model
+./make-tab-yaml.pl Model Phenotype > ../conf/golr-views/single-tab/model-phenotype.yaml
+./make-tab-yaml.pl Model Disease > ../conf/golr-views/single-tab/model-disease.yaml
+./make-tab-yaml.pl Model Gene > ../conf/golr-views/single-tab/model-gene.yaml
+./make-tab-yaml.pl Model Variant > ../conf/golr-views/single-tab/model-variant.yaml
+./make-tab-yaml.pl Model Pathway > ../conf/golr-views/single-tab/model-pathway.yaml
+
+# Gene
+#./make-tab-yaml.pl Gene Phenotype > ../conf/golr-views/single-tab/gene-phenotype.yaml
+./make-tab-yaml.pl Gene Disease > ../conf/golr-views/single-tab/gene-disease.yaml
+./make-tab-yaml.pl Gene Model > ../conf/golr-views/single-tab/gene-model.yaml
+./make-tab-yaml.pl Gene Variant > ../conf/golr-views/single-tab/gene-variant.yaml
+./make-tab-yaml.pl Gene Pathway > ../conf/golr-views/single-tab/gene-pathway.yaml
+./make-tab-yaml.pl Gene Homolog > ../conf/golr-views/single-tab/gene-homolog.yaml
+
---
+++ scripts/make-tab-yaml.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $subject = shift;
+my $object = shift;
+
+my $subject_lc = lc($subject);
+my $object_lc = lc($object);
+
+
+print "id: ${subject_lc}_${object_lc}
+schema_generating: true
+document_category: generic_association
+weight: 20
+
+## Results table, horizontal
+result_weights: subject^7.0 subject_taxon^6.5 relation^6.0  object^5.0 object_taxon^4.2 evidence^4.0 source^3.5
+
+## Ordering of facets, vertical
+filter_weights: relation_closure_label^6.5 evidence_closure_label^6.0 object_closure_label^4.5
+
+## SOLR FIELD DEFINITIONS
+
+fields:
+
+## SUBJECT
+
+  - id: subject
+    description: \"The CURIE for oban:association_has_subject. May be disease, variant, gene, person, ....\"
+    display_name: $subject
+    type: string
+    property: []
+
+  - id: subject_label
+    description: \"Label for association_subject. This will always be rdfs:label. Conventions may vary as to what goes in the label. For genes will be symbol, but we may choose to uniquify by prefixing with species\"
+    display_name: $subject
+    type: string
+    property: []
+    searchable: true
+
+  - id: subject_closure
+    description: \"Reflexive closure of association_has_subject. A list of CURIEs. If an individual, first traverse rdf:tpye. The default closure is subclass, but other may optionally be added depending on what type of association this is and what the class is. E.g. for expression associations, the object is an anatomy class, and the closure will include part_of\"
+    display_name: $subject
+    type: string
+    cardinality: multi
+    property: []
+
+  - id: subject_closure_label
+    description: \"Labels for subject_closure.\"
+    display_name: $subject
+    type: string
+    cardinality: multi
+    property: []
+    searchable: true
+
+  - id: object
+    description: \"The CURIE for oban:association_has_object. This is often, but not always an ontology class. E.g. for a gene-gene interaction it is an (arbitrary) member of the pair.\"
+    display_name: $object
+    type: string
+    property: []
+
+  - id: object_label
+    description: \"Label for association_object.\"
+    display_name: $object
+    type: string
+    property: []
+    searchable: true
+
+  - id: object_closure
+    description: \"Reflexive closure of association_has_object. A list of CURIEs. If an individual, first traverse rdf:tpye. The default closure is subclass, but other may optionally be added depending on what type of association this is and what the class is. E.g. for expression associations, the object is an anatomy class, and the closure will include part_of\"
+    display_name: $object
+    type: string
+    cardinality: multi
+    property: []
+
+  - id: object_closure_label
+    description: \"Labels for object_class_closure.\"
+    display_name: $object
+    type: string
+    cardinality: multi
+    property: []
+    searchable: true";
