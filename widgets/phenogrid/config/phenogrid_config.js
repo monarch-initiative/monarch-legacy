c0eacc7741a324040308114a3ac3e41b8a2b34b9
yuanzhou <yuanzhou19@gmail.com>
2015-07-09 23:05:54 -0400

    added config/phenogrid_config.js

---
+++ widgets/phenogrid/config/phenogrid_config.js
@@ -0,0 +1,13 @@
+var configoptions = {
+    "serverURL": "",
+    "selectedCalculation": "0",
+    "invertAxis": false,
+    "selectedSort": "Frequency",
+    "targetSpeciesName" : "Overview",			   
+    "refSpecies": "Homo sapiens",
+    "targetSpeciesList" : [
+		{"name": "Homo sapiens", "taxon": "9606"},
+		{"name": "Mus musculus", "taxon": "10090"},
+		{"name": "Danio rerio", "taxon": "7955"}
+	]
+};
