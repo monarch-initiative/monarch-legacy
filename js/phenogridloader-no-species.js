d32b9d800e6e236ff06eb1de80d465f63ad1285d
kshefchek <kshefchek@gmail.com>
2015-07-12 11:40:56 -0700

    only load phenogrid when clicking on the compare tab and added to page using puptent, but lost css ajax loader, see #807 and #738

--- js/phenogridloader-no-species.js
+++ js/phenogridloader-no-species.js
@@ -1,17 +1,28 @@
+// Phenogrid loader
+function loadPhenogrid(){
 
-$(function () { 
-    var disease_id = this.location.pathname;
-    var slash_idx = disease_id.indexOf('/');
-    disease_id = disease_id.substring(slash_idx+1);
-    var phenotype_list = [];
-    jQuery.ajax({ 
-    	url : '/' + disease_id + '/phenotype_list.json', 
-    	async : false, 
-    	dataType : 'json', 
-    	success : function(data) {
-    	    phenotype_list = data.phenotype_list;
+    jQuery('#categories a[href="#compare"]').click(function(event) {
+        if (!(jQuery('#pg_svg_container').length)){
+            initPhenogrid();
         }
     });
 
-    $("#phen_vis").phenogrid({phenotypeData: phenotype_list});
-});        
+
+    function initPhenogrid () { 
+        var disease_id = this.location.pathname;
+        var slash_idx = disease_id.indexOf('/');
+        disease_id = disease_id.substring(slash_idx+1);
+        var phenotype_list = [];
+        jQuery.ajax({ 
+            url : '/' + disease_id + '/phenotype_list.json', 
+            async : false, 
+            dataType : 'json', 
+            success : function(data) {
+                phenotype_list = data.phenotype_list;
+            }
+        });
+
+        $("#phen_vis").phenogrid({phenotypeData: phenotype_list});
+    }
+
+}
