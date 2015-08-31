3bfcbd5a504765b6a6984d1ac1c881c0850abafe
kshefchek <kshefchek@gmail.com>
2015-08-31 09:30:00 -0700

    ontology browser fix label bug

--- js/lib/monarch/widget/browse.js
+++ js/lib/monarch/widget/browse.js
@@ -201,6 +201,9 @@ bbop.monarch.widget.browse = function(server, manager, reference_id, interface_i
                   lbl = lbl.replace(/\b[a-z]/g, function() {
                       return arguments[0].toUpperCase()});
                   lbl = lbl.replace(/Abnormal\(Ly\)/,'Abnormal(ly)');
+                  if (!lbl){
+                      lbl = level_item[1];
+                  }
                   var rel = level_item[2];
                   
                   // For various sections, decide to run image
