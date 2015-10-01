93d281c1c3a9ab7b73e5ca843259d96ac892278d
kshefchek <kshefchek@gmail.com>
2015-10-01 08:34:58 -0700

    adds taxon to autocomplete and search, removes duplicate classes in autocomplete, see #865, #861

--- js/search_form.js
+++ js/search_form.js
@@ -92,19 +92,141 @@ function navbar_search_init(in_search_id, in_form_id){
 		    var pared_data = [];
 		    var seen_ids = {};
 		    for( var di = 0; di < data.length; di++ ){
-			var datum = data[di];
-			var datum_id = datum['id'];
-			if( ! seen_ids[datum_id] ){
-			    // Only add new ids to pared data list.
-			    pared_data.push(datum);
-			    
-			    // Block them in the future.
-			    seen_ids[datum_id] = true;
-			}
+		        var datum = data[di];
+		        var datum_id = datum['id'];
+		        if( ! seen_ids[datum_id] ){
+		            // Only add new ids to pared data list.
+		            pared_data.push(datum);
+		            
+		            // Block them in the future.
+		            seen_ids[datum_id] = true;
+		        }
 		    }
 
 		    var map = jQuery.map(pared_data, _parse_data_item);
-		    response(map);
+
+		    if (map.length > 0) {
+		        var id_list = map.map( function(i) { return i.id; });
+		        var ids = id_list.join('&id=');
+		        //TODO pass server in using puptent var
+		        var qurl = "http://rosie.crbs.ucsd.edu:9000/scigraph/graph/neighbors?id=" 
+		            + ids + "&depth=1&blankNodes=false&relationshipType=http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FRO_0002162"
+		            + "&direction=BOTH&project=%2A";
+		        jQuery.ajax({
+		            url: qurl,
+		            dataType:"json",
+		            error: function (){
+		                console.log('could not get taxon for genes');
+		                response(map);
+		            },
+		            success: function ( data ){
+		                var graph = new bbop.model.graph();
+		                graph.load_json(data);
+		                id_list.forEach(function (id) {
+		                    var label = '';
+		                    var taxon_list = graph.get_parent_nodes(id, 'http://purl.obolibrary.org/obo/RO_0002162');
+		                    if (taxon_list && taxon_list.length > 0){
+	                            label = taxon_list[0].label();
+	                            var meta = taxon_list[0].metadata();
+	                            if (meta && meta['synonym']){
+	                                label = meta['synonym'][0];
+	                            }
+	                            label = label.replace(/\b[a-z]/g, function() {
+	                                return arguments[0].toUpperCase()
+	                            });
+	                        }
+		                    for (i = 0; i < map.length; i++){
+	                            if (map[i]['id'] == id) {
+	                                if (label) {
+	                                    map[i]['tag'] = label;
+	                                }
+	                            }
+	                        }
+                        });
+		              //TODO pass server in using puptent var
+		                var qurl = "http://geoffrey.crbs.ucsd.edu:9000/scigraph/graph/neighbors?id=" 
+		                    + ids + "&depth=3&blankNodes=false&relationshipType=equivalentClass"
+		                    + "&direction=BOTH&project=%2A";
+		                jQuery.ajax({
+		                    url: qurl,
+		                    dataType:"json",
+		                    error: function (){
+		                        console.log('error fetching equivalencies');
+		                        response(map);
+		                    },
+		                    success: function ( data ){
+		                        var equivalent_graph = new bbop.model.graph();
+		                        equivalent_graph.load_json(data);
+		                        
+		                        //Need to put this in one place
+		                        equivalent_graph.get_descendent_subgraph = function(obj_id, pred){   
+		                            var anchor = this;
+		                            var edge_list = new Array();
+		                            var descendent_graph = new bbop.model.graph();
+		                            if (typeof anchor.seen_node_list === 'undefined') {
+		                                anchor.seen_node_list = [obj_id];
+		                            }
+		                            
+		                            anchor.get_child_nodes(obj_id, pred).forEach( function(sub_node) {
+		                                var sub_id = sub_node.id();
+		                                if (anchor.seen_node_list.indexOf(sub_id) > -1){
+		                                    return;
+		                                }
+		                                anchor.seen_node_list.push(sub_id);
+		                                descendent_graph.add_edge(anchor.get_edge(sub_id, obj_id, pred));
+		                                descendent_graph.add_node(anchor.get_node(sub_id));
+		                                descendent_graph.add_node(anchor.get_node(obj_id));
+		                                descendent_graph.merge_in(anchor.get_descendent_subgraph(sub_id, pred));
+		                            });
+		                                
+		                            return descendent_graph; 
+		                        }
+		                        
+		                        for (var i=0; i < map.length; i++) {
+		                            var id = map[i]['id'];
+		                            var eq_node_list = [];
+		                            //Get all equivalent nodes of v[i][0]
+		                            var equivalent_nodes = equivalent_graph.get_ancestor_subgraph(id, 'equivalentClass')
+		                            .all_nodes();
+		                            var other_eq_nodes = equivalent_graph.get_descendent_subgraph(id, 'equivalentClass')
+		                            .all_nodes();
+		                            
+		                            eq_node_list = equivalent_nodes.map(function(i){return i.id();});
+		                            var temp_list = other_eq_nodes.map(function(i){return i.id();});
+		                            
+		                            eq_node_list.push.apply(eq_node_list, temp_list);
+		                            //equivalent_node_list.map
+		                            
+		                            for (var k=i+1; k < map.length; k++) {
+		                                var node_id = map[k]['id'];
+		                                if (node_id) {
+		                                    console.log('comparing id: ' + id + ' to: ' + node_id);
+		                                    if (eq_node_list.indexOf(node_id) > -1){
+		                                        
+		                                        // If the id is from MESH
+		                                        if (/^MESH/.test(id)){
+		                                            console.log('removing equivalent node '+ id);
+		                                            map.splice(i,1)
+		                                            i--;
+		                                            break;
+		                                        } else {
+		                                            console.log('removing equivalent node '+node_id);
+		                                            map.splice(k, 1);
+		                                            k--;
+		                                            continue;
+		                                        }
+		                                    }
+		                                }
+		                        
+		                            }
+		                        }
+		                        response(map);  
+		                    }   
+		                });
+		            
+		        }
+		        });
+		    }
 		};
 
 		var query = "/autocomplete/"+request.term+".json";
