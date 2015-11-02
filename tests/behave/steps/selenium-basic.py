c916f04d516428f5eeaf6afe83136ac0c325a9cc
kshefchek <kshefchek@gmail.com>
2015-11-02 14:33:42 -0800

    uncomment out logs for behave tests

--- tests/behave/steps/selenium-basic.py
+++ tests/behave/steps/selenium-basic.py
@@ -59,13 +59,13 @@ def step_impl(context):
 def step_impl(context, text):
     print(context.browser.title)
     webelt = context.browser.find_element_by_tag_name('html')
-    # print("###### text: %s" % text)
-    # print("###### webelt.text: %s" % webelt.text)
-    # print("###### rfind: %d" % webelt.text.rfind(text))
+    print("###### text: %s" % text)
+    print("###### webelt.text: %s" % webelt.text)
+    print("###### rfind: %d" % webelt.text.rfind(text))
     assert webelt.text.rfind(text) != -1
-    # webelt = context.browser.find_element_by_tag_name('body')
-    # print(webelt.get_attribute('innerHTML'))
-    # assert webelt.get_attribute('innerHTML').rfind(text) != -1
+    webelt = context.browser.find_element_by_tag_name('body')
+    print(webelt.get_attribute('innerHTML'))
+    assert webelt.get_attribute('innerHTML').rfind(text) != -1
 
 ## The document body should contain a certain piece of text.
 @then("the document should contain '{text}'")
