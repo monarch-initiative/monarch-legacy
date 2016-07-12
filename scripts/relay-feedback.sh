a68fa4a184947d58bb359634999dc98534698f1f
DoctorBud <dan@quantumclay.com>
2016-07-12 11:04:07 -0700

    Fixes Feedback window, which has been broken since January. Adds a scripts/relay-feedback.sh script which can be used to copy the feedback file to a central location. Fixes styling of close boxes on feedback dialog and response alert. adjusts position of alert to be visible.

---
+++ scripts/relay-feedback.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Script to be invoked by cron or Jenkins (or manually) for the purpose
+# of copying the accumulated feedback.txt file to a more central and persistent location
+#
+# Dependencies:
+# - SSH pubkey for the invoking server (monarch-app-beta or monarch-app-prod) must be installed
+#   on the TARGETUSER@TARGETHOST, in the SSH authorized_keys file
+# - There must be a ~/feedback/ directory present on the target
+#
+# Example crontab:
+#       SHELL=/bin/bash
+#       MAILTO=""
+#       # m h dom mon dow usercommand
+#       * * * * * /opt/monarch-app/scripts/relay-feedback.sh        # Runs every minute
+#       */5 * * * * /opt/monarch-app/scripts/relay-feedback.sh      # Runs every 5 minutes
+#       0 */2 * * * /opt/monarch-app/scripts/relay-feedback.sh      # Runs every two hours
+#       0 */6 * * * /opt/monarch-app/scripts/relay-feedback.sh      # Runs every six hours (recommended)
+#
+
+FEEDBACKFILE=/opt/monarch-app/feedback.txt
+HOST=$(hostname)
+TIMESTAMP=$(date "+%Y.%m.%d-%H.%M.%S")
+TARGETHOST=monarch4-2.cslu.ohsu.edu
+TARGETUSER=dkeith
+TARGETFILE=feedback/${HOST}-${TIMESTAMP}.txt
+TARGETSCP=${TARGETUSER}@${TARGETHOST}:${TARGETFILE}
+
+if test -f "${FEEDBACKFILE}"; then
+    scp ${FEEDBACKFILE} ${TARGETSCP}
+fi
