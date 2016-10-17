1679ec791298402fce8832dd8b824475e5a417c4
Zhou Yuan <yuanzhou19@gmail.com>
2016-10-17 17:06:18 -0400

    working demo of isotope

--- js/search_results.js
+++ js/search_results.js
@@ -11,46 +11,49 @@ var groupsByTaxon = _.groupBy(searchResults, "taxon");
 
 console.log(groupsByTaxon);
 
-
-
 // isotope
 // external js: isotope.pkgd.js
 
-// init Isotope
-var $grid = $('.grid').isotope({
-  itemSelector: '.color-shape'
+$( document ).ready(function() {
+    
+    // init Isotope
+    var $grid = $('.grid').isotope({
+      itemSelector: '.color-shape'
+    });
+
+    // store filter for each group
+    var filters = {};
+
+    $('.filters').on( 'click', '.search-results-button', function() {
+      var $this = $(this);
+      // get group key
+      var $buttonGroup = $this.parents('.search-results-button-group');
+      var filterGroup = $buttonGroup.attr('data-filter-group');
+      // set filter for group
+      filters[ filterGroup ] = $this.attr('data-filter');
+      // combine filters
+      var filterValue = concatValues( filters );
+      // set filter for Isotope
+      $grid.isotope({ filter: filterValue });
+    });
+
+    // change is-checked class on buttons
+    $('.button-group').each( function( i, buttonGroup ) {
+      var $buttonGroup = $( buttonGroup );
+      $buttonGroup.on( 'click', 'button', function() {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $( this ).addClass('is-checked');
+      });
+    });
+      
+    // flatten object by concatting values
+    function concatValues( obj ) {
+      var value = '';
+      for ( var prop in obj ) {
+        value += obj[ prop ];
+      }
+      return value;
+    }
+
 });
 
-// store filter for each group
-var filters = {};
-
-$('.filters').on( 'click', '.button', function() {
-  var $this = $(this);
-  // get group key
-  var $buttonGroup = $this.parents('.button-group');
-  var filterGroup = $buttonGroup.attr('data-filter-group');
-  // set filter for group
-  filters[ filterGroup ] = $this.attr('data-filter');
-  // combine filters
-  var filterValue = concatValues( filters );
-  // set filter for Isotope
-  $grid.isotope({ filter: filterValue });
-});
-
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
-});
-  
-// flatten object by concatting values
-function concatValues( obj ) {
-  var value = '';
-  for ( var prop in obj ) {
-    value += obj[ prop ];
-  }
-  return value;
-}
