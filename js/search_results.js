e9dbff2b177302765f5546f23e0be85d259df453
Zhou Yuan <yuanzhou19@gmail.com>
2016-10-17 16:12:44 -0400

    Using underscore to group search results

--- js/search_results.js
+++ js/search_results.js
@@ -1,1 +1,12 @@
+// searchResults is an array
 console.log(searchResults);
+
+// group the results by category
+var groupsByCategory = _.groupBy(searchResults, "category");
+
+console.log(groupsByCategory);
+
+// group the results by taxon
+var groupsByTaxon = _.groupBy(searchResults, "taxon");
+
+console.log(groupsByTaxon);
