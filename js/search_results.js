7dc365c406eaa89c7d661edda9efbfc64ed4e118
Zhou Yuan <yuanzhou19@gmail.com>
2016-10-24 13:09:27 -0400

    Filtering based on table view

--- js/search_results.js
+++ js/search_results.js
@@ -1,49 +1,25 @@
 // searchResults is an array
 console.log(searchResults);
 
-// isotope
-// external js: isotope.pkgd.js
 
 $( document ).ready(function() {
-    
-    // init Isotope
-    var $grid = $('.search-results-grid').isotope({
-      itemSelector: '.search-result-box'
-    });
-
-    // store filter for each group
-    var filters = {};
-
-    $('.filters').on( 'click', '.search-results-button', function() {
-      var $this = $(this);
-      // get group key
-      var $buttonGroup = $this.parents('.search-results-button-group');
-      var filterGroup = $buttonGroup.attr('data-filter-group');
-      // set filter for group
-      filters[ filterGroup ] = $this.attr('data-filter');
-      // combine filters
-      var filterValue = concatValues( filters );
-      // set filter for Isotope
-      $grid.isotope({ filter: filterValue });
-    });
 
     // change is-checked class on buttons
     $('.search-results-button-group').each( function( i, buttonGroup ) {
       var $buttonGroup = $( buttonGroup );
-      $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.on('click', 'button', function() {
         $buttonGroup.find('.is-checked').removeClass('is-checked');
-        $( this ).addClass('is-checked');
+        $(this).addClass('is-checked');
+        
+        // hide
+        var filterClass = $(this).data("filter");
+        $('.search-result-item').filter(filterClass).hide();
+
+
       });
     });
-      
-    // flatten object by concatting values
-    function concatValues( obj ) {
-      var value = '';
-      for ( var prop in obj ) {
-        value += obj[ prop ];
-      }
-      return value;
-    }
+
+    
 
 });
 
