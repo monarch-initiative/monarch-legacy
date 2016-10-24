004341f6b72d6905ee0adbe299e7e7eea1bf3136
Zhou Yuan <yuanzhou19@gmail.com>
2016-10-24 10:58:02 -0400

    rebased on master and rendering UI using new API

--- js/search_results.js
+++ js/search_results.js
@@ -1,16 +1,6 @@
 // searchResults is an array
 console.log(searchResults);
 
-// group the results by category
-var groupsByCategory = _.groupBy(searchResults, "category");
-
-console.log(groupsByCategory);
-
-// group the results by taxon
-var groupsByTaxon = _.groupBy(searchResults, "taxon");
-
-console.log(groupsByTaxon);
-
 // isotope
 // external js: isotope.pkgd.js
 
