5bcffd9165d9e428c20146026a4d695ae935859e
Zhou Yuan <yuanzhou19@gmail.com>
2016-11-03 09:36:08 -0400

    Formatted code and commented out console.log()

--- js/search_results.js
+++ js/search_results.js
@@ -1,5 +1,5 @@
 // searchResults is an array
-console.log(searchResults);
+//console.log(searchResults);
 
 
 $( document ).ready(function() {
@@ -37,7 +37,53 @@ $( document ).ready(function() {
       // E.g. {"category": ["gene"], "taxon_label": ["Danio rerio"]}
       // Must use "/" at the beginning of the URL
       var url = '/searchfiltering/' + searchTerm + '/' + JSON.stringify(filters) + '/' + pageNum;
-      console.log(url);
+      //console.log(url);
+
+      // fire the new search call and update table
+      // Separate the ajax request with callbacks
+      var jqxhr = $.ajax({
+        url: url,
+        method: 'GET', 
+        async : true,
+        dataType : 'json'
+      });
+
+      jqxhr.done(function(data) {
+        //console.log(data);
+
+        if (typeof(data) !== 'undefined') {
+          // update the table with this new table content
+          $('.search-results-rows').html(data.table);
+
+          // Update the total count number
+          $('#totalCount').html(data.count);
+
+          if (data.loadMoreBtn === false) {
+            // Hide the load mroe button
+            $('#more').hide();
+          } else {
+            $('#more').show();
+          }
+        }
+      });
+
+      jqxhr.fail(function () { 
+        console.log('Ajax error!')
+      });
+
+    });
+
+
+    // Load more content
+    $('#more').click(function(){
+      // Increase the page number
+      pageNum++;
+      //console.log(pageNum);
+
+      // Convert object to JSON string
+      // E.g. {"category": ["gene"], "taxon_label": ["Danio rerio"]}
+      // Must use "/" at the beginning of the URL
+      var url = '/searchfiltering/' + searchTerm + '/' + JSON.stringify(filters) + '/' + pageNum;
 
       // fire the new search call and update table
       // Separate the ajax request with callbacks
@@ -49,83 +95,34 @@ $( document ).ready(function() {
       });
       
       jqxhr.done(function(data) {
-        console.log(data);
-        
+        //console.log(data);
+
         if (typeof(data) !== 'undefined') {
-              // update the table with this new table content
-              $('.search-results-rows').html(data.table);
+          // append new table content
+          $('.search-results-rows').append(data.table);
+        }
 
-              // Update the total count number
-              $('#totalCount').html(data.count);
-
-              if (data.loadMoreBtn === false) {
-                  // Hide the load mroe button
-                  $('#more').hide();
-                } else {
-                  $('#more').show();
-                }
-              }
-            });
+        if (data.loadMoreBtn === false) {
+          // Hide the load mroe button
+          $('#more').hide();
+        } else {
+          $('#more').show();
+        }
+      });
       
       jqxhr.fail(function () { 
-        console.log('Ajax error!')
+        console.log('Ajax error to fetch more results!')
       });
-
     });
 
 
-    // Load more content
-    $('#more').click(function(){
-        // Increase the page number
-        pageNum++;
-        console.log(pageNum);
-
-        // Convert object to JSON string
-        // E.g. {"category": ["gene"], "taxon_label": ["Danio rerio"]}
-        // Must use "/" at the beginning of the URL
-        var url = '/searchfiltering/' + searchTerm + '/' + JSON.stringify(filters) + '/' + pageNum;
-
-        // fire the new search call and update table
-        // Separate the ajax request with callbacks
-        var jqxhr = $.ajax({
-          url: url,
-          method: 'GET', 
-          async : true,
-          dataType : 'json'
-        });
-        
-        jqxhr.done(function(data) {
-          console.log(data);
-          
-          if (typeof(data) !== 'undefined') {
-              // append new table content
-              $('.search-results-rows').append(data.table);
-            }
-
-            if (data.loadMoreBtn === false) {
-                // Hide the load mroe button
-                $('#more').hide();
-              } else {
-                $('#more').show();
-              }
-            });
-        
-        jqxhr.fail(function () { 
-          console.log('Ajax error to fetch more results!')
-        });
-      });
-
-
-
-
     // change is-checked class on buttons
-    $('.search-results-button-group').each( function( i, buttonGroup ) {
+    $('.search-results-button-group').each(function(i, buttonGroup) {
       var $buttonGroup = $( buttonGroup );
       $buttonGroup.on('click', 'button', function() {
         // Highlight selected button
         $buttonGroup.find('.is-checked').removeClass('is-checked');
         $(this).addClass('is-checked');
-
       });
     });
 
