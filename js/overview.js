d4765d3ebd818980b265506b4938aea5e7d91fe5
Nathan Dunn <ndunn@me.com>
2017-09-29 16:49:45 -0700

    added d3 hooks and it shows up

--- js/overview.js
+++ js/overview.js
@@ -8,43 +8,47 @@
  */
 var Q = require('q');
 var MonarchCommon = require('./monarch-common.js');
+import {scaleLinear} from 'd3-scale';
+import {axisTop} from 'd3-axis';
+import {select} from 'd3-selection';
 
 function launchBrowser(id, root, reference_id, reference_label) {
 
-        // Conf
-        // Global scigraph url passed in from webapp.js addCoreRenderers
-        var srv = global_scigraph_url;
+    // Conf
+    // Global scigraph url passed in from webapp.js addCoreRenderers
+    var srv = global_scigraph_url;
 
-        var manager = new bbop.rest.manager.jquery(bbop.rest.response.json);
+    var manager = new bbop.rest.manager.jquery(bbop.rest.response.json);
 
-        if (!root){
-            root = "HP:0000118";
-        }
+    if (!root) {
+        root = "HP:0000118";
+    }
 
-        // Browser.
-        var browser = new bbop.monarch.widget.browse(srv, manager, id, root, 'brw', reference_id, reference_label, {
-            'info_icon': 'info',
-            'current_icon': 'current_term',
-            'base_icon_url': '/image',
-            'image_type': 'gif',
-            'info_button_callback':
-                function(term_acc, term_doc){
-                    // // Local form.
-                    // shield.draw(term_doc);
-                    // Remote form (works).
-                    //shield.draw(term_acc);
-                }
-        });
+    // Browser.
+    var browser = new bbop.monarch.widget.browse(srv, manager, id, root, 'brw', reference_id, reference_label, {
+        'info_icon': 'info',
+        'current_icon': 'current_term',
+        'base_icon_url': '/image',
+        'image_type': 'gif',
+        'info_button_callback':
+            function (term_acc, term_doc) {
+                // // Local form.
+                // shield.draw(term_doc);
+                // Remote form (works).
+                //shield.draw(term_acc);
+            }
+    });
 
-        browser.init_browser(id);
+    browser.init_browser(id);
 }
 
-function getOntologyBrowser(id, label, root){
+function getOntologyBrowser(id, label, root) {
 
-    var spinner_args = {'generate_id': true,
-            'class':
+    var spinner_args = {
+        'generate_id': true,
+        'class':
             'progress progress-striped active',
-            'style': 'width: 3em; display:inline-block; margin-top:3px; margin-left:10px;'
+        'style': 'width: 3em; display:inline-block; margin-top:3px; margin-left:10px;'
     };
     var spinner = makeSpinnerDiv(spinner_args);
 
@@ -55,12 +59,12 @@ function getOntologyBrowser(id, label, root){
 
     jQuery.ajax({
         url: qurl,
-        dataType:"json",
-        error: function (){
+        dataType: "json",
+        error: function () {
             console.log('error fetching clique leader');
             launchBrowser(id, root);
         },
-        success: function ( data ){
+        success: function (data) {
             var graph = new bbop.model.graph();
             graph.load_json(data);
             var node_list = [];
@@ -82,11 +86,11 @@ function fetchLiteratureOverview(id) {
     jQuery('#overview').append(spinner.to_string());
 
     var parseEFetch = function (data) {
-        jQuery('#'+spinner.get_id()).remove();
+        jQuery('#' + spinner.get_id()).remove();
         var xml = jQuery(data);
 
         //Get abstract text and add to DOM
-        var abstractText = xml.find( "AbstractText" );
+        var abstractText = xml.find("AbstractText");
         var abstractElt = '<p><span style=\"font-weight:bold\">Abstract</span>: '
             + abstractText.text() + '</p>';
         jQuery("#overview").append(abstractElt);
@@ -95,7 +99,7 @@ function fetchLiteratureOverview(id) {
         var meshHeadings = xml.find("MeshHeading");
         //console.log(meshHeadings);
         var meshTerms = [];
-        meshHeadings.each( function (i, heading) {
+        meshHeadings.each(function (i, heading) {
             var headXML = jQuery(heading);
             meshTerms.push(headXML.children("DescriptorName").text());
         });
@@ -105,7 +109,7 @@ function fetchLiteratureOverview(id) {
         jQuery("#overview").append(meshElt);
     };
     var onError = function (data) {
-        jQuery('#'+spinner.get_id()).remove();
+        jQuery('#' + spinner.get_id()).remove();
         console.log("Error fetching from ncbi EFetch Service");
     };
 
@@ -169,23 +173,66 @@ function fetchGeneDescription(geneID) {
             jQuery('#' + spinner.get_id()).remove();
         },
         success(data) {
-            jQuery('#'+spinner.get_id()).remove();
-            if(data.hits.length > 0 && 'genomic_pos' in data.hits[0]){
+            jQuery('#' + spinner.get_id()).remove();
+            if (data.hits.length > 0 && 'genomic_pos' in data.hits[0]) {
                 var hit = data.hits[0];
-                var symbol = hit.symbol ;
+                var symbol = hit.symbol;
                 var locationObj = hit.genomic_pos;
-                if(locationObj){
+                if (locationObj) {
                     var thisSpecies = speciesParam;
                     var defaultTrackName = 'All Genes'; // this is the generic track name
-                    var locationString = locationObj.chr+ ':' + locationObj.start+ '..' + locationObj.end;
+                    var locationString = locationObj.chr + ':' + locationObj.start + '..' + locationObj.end;
                     var apolloServerPrefix = 'https://agr-apollo.berkeleybop.io/apollo/';
                     var trackDataPrefix = apolloServerPrefix + 'track/' + encodeURI(thisSpecies) + '/' + defaultTrackName + '/' + encodeURI(locationString) + '.json';
                     var trackDataWithHighlight = trackDataPrefix + '?name=' + symbol;
-                    var trackDataElt = '<a href="' + trackDataWithHighlight +'">link</a>';
+                    var trackDataElt = '<a href="' + trackDataWithHighlight + '">link</a>';
                     jQuery('#mygene-feature').append(trackDataElt);
+
+                    jQuery.ajax({
+                        url: trackDataWithHighlight,
+                        dataType: 'json',
+                        error() {
+                            console.log('Failed to fetch the genome feature data');
+                        },
+                        success(data) {
+                            var svgDataElt = '' +
+                                '<svg id="genome-feature">' +
+                                +'</svg>';
+                            jQuery('#mygene-feature').append(svgDataElt);
+                            // var genomeFeature =window.document.getElementById('genome-feature') ;
+                            // let calculatedHeight = this.props.height;
+                            // if (!this.props.isLoading) {
+                            //     let numberIsoforms = this.countIsoforms(this.props.data);
+                            //     if (numberIsoforms > this.MAX_ISOFORMS) {
+                            //         calculatedHeight = (this.MAX_ISOFORMS + 2) * isoform_height;
+                            //     }
+                            //     else {
+                            //         calculatedHeight = (numberIsoforms + 1) * isoform_height;
+                            //     }
+                            // }
+                            let calculatedHeight = 300;
+                            let margin = {top: 8, right: 30, bottom: 30, left: 40},
+                                width = 960 - margin.left - margin.right,
+                                height = calculatedHeight - margin.top - margin.bottom;
+                            let viewer = select('#genome-feature')
+                                    .attr('width', width + margin.left + margin.right)
+                                    .attr('height', height + margin.top + margin.bottom)
+                                    .append('g')
+                                    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+                            let isoform_title_height = 30;
+                            viewer.append('text')
+                                .attr('x',  30)
+                                .attr('y',  isoform_title_height+10 )
+                                .attr('fill', 'orange')
+                                .attr('opacity', 0.6 )
+                                // .attr('height', isoform_title_height)
+                                .text('Overview of non-coding genome features unavailable at this time.');
+
+                        }
+                    });
                 }
             }
-            else{
+            else {
                 console.log('fetchGeneDescription. No Genome Features fetched from mygene');
                 jQuery('#mygene-feature').hide();
                 jQuery('#' + spinner.get_id()).remove();
@@ -194,8 +241,8 @@ function fetchGeneDescription(geneID) {
             if (data.hits.length > 0 && 'summary' in data.hits[0]) {
                 var hit = data.hits[0];
                 var summary = hit.summary;
-                var summaryElt = "<span>" + summary + ' [Retrieved from '+
-                '<a href="' +
+                var summaryElt = "<span>" + summary + ' [Retrieved from ' +
+                    '<a href="' +
                     serviceURL + '?q=' + formattedID + '&fields=summary&species=all' +
                     '">Mygene.info</a>]</span>';
                 jQuery('#mygene-description').append(summaryElt);
