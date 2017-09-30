e7930dece54039c1475d7b81b1afa33cabce5196
Nathan Dunn <ndunn@me.com>
2017-09-29 17:09:46 -0700

    added styling and looks great

--- js/overview.js
+++ js/overview.js
@@ -117,6 +117,43 @@ function fetchLiteratureOverview(id) {
 
 }
 
+function countIsoforms(data) {
+
+    let isoform_count = 0;
+    // gene level
+    for (let i in data) {
+        let feature = data[i];
+        feature.children.forEach(function (geneChild) {
+            // isoform level
+            if (geneChild.type == 'mRNA') {
+                isoform_count += 1;
+            }
+        });
+    }
+    return isoform_count;
+}
+
+function findRange(data) {
+    let fmin = -1;
+    let fmax = -1;
+
+    for (let d in data) {
+        if (fmin < 0 || data[d].fmin < fmin) {
+            fmin = data[d].fmin;
+        }
+        if (fmax < 0 || data[d].fmax > fmax) {
+            fmax = data[d].fmax;
+        }
+    }
+
+
+    return {
+        fmin: fmin
+        , fmax: fmax
+    };
+}
+
+
 function fetchGeneDescription(geneID) {
     //https://mygene.info/v2/query?q=6469&fields=summary
     const spinnerArgs = {
@@ -185,8 +222,8 @@ function fetchGeneDescription(geneID) {
                     var apolloServerPrefix = 'https://agr-apollo.berkeleybop.io/apollo/';
                     var trackDataPrefix = apolloServerPrefix + 'track/' + encodeURI(thisSpecies) + '/' + defaultTrackName + '/' + encodeURI(locationString) + '.json';
                     var trackDataWithHighlight = trackDataPrefix + '?name=' + symbol;
-                    var trackDataElt = '<a href="' + trackDataWithHighlight + '">link</a>';
-                    jQuery('#mygene-feature').append(trackDataElt);
+                    // var trackDataElt = '<a href="' + trackDataWithHighlight + '">link</a>';
+                    // jQuery('#mygene-feature').append(trackDataElt);
 
                     jQuery.ajax({
                         url: trackDataWithHighlight,
@@ -199,34 +236,171 @@ function fetchGeneDescription(geneID) {
                                 '<svg id="genome-feature">' +
                                 +'</svg>';
                             jQuery('#mygene-feature').append(svgDataElt);
-                            // var genomeFeature =window.document.getElementById('genome-feature') ;
-                            // let calculatedHeight = this.props.height;
-                            // if (!this.props.isLoading) {
-                            //     let numberIsoforms = this.countIsoforms(this.props.data);
-                            //     if (numberIsoforms > this.MAX_ISOFORMS) {
-                            //         calculatedHeight = (this.MAX_ISOFORMS + 2) * isoform_height;
-                            //     }
-                            //     else {
-                            //         calculatedHeight = (numberIsoforms + 1) * isoform_height;
-                            //     }
-                            // }
-                            let calculatedHeight = 300;
+
+
+                            let dataRange = findRange(data);
+                            let view_start = dataRange.fmin;
+                            let view_end = dataRange.fmax;
+                            let exon_height = 10; // will be white / transparent
+                            let cds_height = 10; // will be colored in
+                            let isoform_height = 40; // height for each isoform
+                            let isoform_view_height = 20; // height for each isoform
+                            let isoform_title_height = 0; // height for each isoform
+                            let utr_height = 4; // this is the height of the isoform running all of the way through
+                            let arrow_height = 20;
+                            let arrow_width = 10;
+                            let arrow_points = '0,0 0,' + arrow_height + ' ' + arrow_width + ',' + arrow_width;
+                            let calculatedHeight = '600px';
+                            let numberIsoforms = countIsoforms(data);
+                            if (numberIsoforms > this.MAX_ISOFORMS) {
+                                calculatedHeight = (this.MAX_ISOFORMS + 2) * isoform_height;
+                            }
+                            else {
+                                calculatedHeight = (numberIsoforms + 1) * isoform_height;
+                            }
                             let margin = {top: 8, right: 30, bottom: 30, left: 40},
                                 width = 960 - margin.left - margin.right,
                                 height = calculatedHeight - margin.top - margin.bottom;
+
+                            // MEAT here
+                            let x = scaleLinear()
+                                .domain([view_start, view_end])
+                                .range([0, width]);
+
+                            let tickFormat = x.tickFormat(5, '.2s');
+
+                            let xAxis = axisTop(x)
+                                .ticks(10, 's')
+                                .tickSize(8)
+                                .tickFormat(tickFormat);
+
                             let viewer = select('#genome-feature')
-                                    .attr('width', width + margin.left + margin.right)
-                                    .attr('height', height + margin.top + margin.bottom)
-                                    .append('g')
-                                    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-                            let isoform_title_height = 30;
-                            viewer.append('text')
-                                .attr('x',  30)
-                                .attr('y',  isoform_title_height+10 )
-                                .attr('fill', 'orange')
-                                .attr('opacity', 0.6 )
-                                // .attr('height', isoform_title_height)
-                                .text('Overview of non-coding genome features unavailable at this time.');
+                                .attr('width', width + margin.left + margin.right)
+                                .attr('height', height + margin.top + margin.bottom)
+                                .append('g')
+                                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+                            console.log(data);
+
+                            let isoform_count = 0;
+                            let MAX_ISOFORMS = 10;
+                            for (let i in data) {
+
+                                let feature = data[i];
+                                let featureChildren = feature.children;
+                                let selected = feature.selected;
+                                console.log(featureChildren.length);
+
+                                let externalUrl = 'http://abcd.com';
+                                featureChildren.forEach(function (featureChild) {
+                                    let featureType = featureChild.type;
+                                    console.log(featureType);
+                                    if (featureType == 'mRNA') {
+                                        if (isoform_count < MAX_ISOFORMS) {
+                                            isoform_count += 1;
+
+                                            viewer.append('polygon')
+                                                .attr('class', 'GF trans_arrow')
+                                                .attr('points', arrow_points)
+                                                .attr('transform', function () {
+                                                    if (feature.strand > 0) {
+                                                        return 'translate(' + Number(x(feature.fmax)) + ',' + Number((isoform_view_height / 2.0) - (arrow_height / 2.0) + (isoform_height * isoform_count) + isoform_title_height) + ')';
+                                                    }
+                                                    else {
+                                                        return 'translate(' + Number(x(feature.fmin)) + ',' + Number((isoform_view_height / 2.0) + (arrow_height / 2.0) + (isoform_height * isoform_count) + isoform_title_height) + ') rotate(180)';
+                                                    }
+                                                });
+
+                                            viewer.append('rect')
+                                                .attr('class', 'GF UTR')
+                                                .attr('x', x(feature.fmin))
+                                                .attr('y', isoform_height * isoform_count + isoform_title_height)
+                                                .attr('transform', 'translate(0,' + ( (isoform_view_height / 2.0) - (utr_height / 2.0)) + ')')
+                                                .attr('height', utr_height)
+                                                .attr('width', x(feature.fmax) - x(feature.fmin));
+
+                                            viewer.append('text')
+                                                .attr('class', 'GF transcriptLabel')
+                                                .attr('x', x(feature.fmin) + 30)
+                                                .attr('y', isoform_height * isoform_count + isoform_title_height)
+                                                .attr('fill', selected ? 'sandybrown' : 'gray')
+                                                .attr('opacity', selected ? 1 : 0.5)
+                                                .attr('height', isoform_title_height)
+                                                .text(featureChild.name);
+
+                                            // have to sort this so we draw the exons BEFORE the CDS
+                                            featureChild.children = featureChild.children.sort(function (a, b) {
+                                                if (a.type == 'exon' && b.type != 'exon') {
+                                                    return -1;
+                                                }
+                                                else if (a.type == 'CDS' && b.type != 'CDS') {
+                                                    return 1;
+                                                }
+                                                else {
+                                                    return a - b;
+                                                }
+                                            });
+
+                                            featureChild.children.forEach(function (innerChild) {
+                                                let innerType = innerChild.type;
+                                                if (innerType == 'exon') {
+                                                    viewer.append('rect')
+                                                        .attr('class', 'GF exon')
+                                                        .attr('x', x(innerChild.fmin))
+                                                        .attr('y', isoform_height * isoform_count + isoform_title_height)
+                                                        .attr('transform', 'translate(0,' + ( (isoform_view_height / 2.0) - (exon_height / 2.0)) + ')')
+                                                        .attr('height', exon_height)
+                                                        .attr('z-index', 10)
+                                                        .attr('width', x(innerChild.fmax) - x(innerChild.fmin));
+                                                }
+                                                else if (innerType == 'CDS') {
+                                                    viewer.append('rect')
+                                                        .attr('class', 'GF CDS')
+                                                        .attr('x', x(innerChild.fmin))
+                                                        .attr('y', isoform_height * isoform_count + isoform_title_height)
+                                                        .attr('transform', 'translate(0,' + ( (isoform_view_height / 2.0) - (cds_height / 2.0)) + ')')
+                                                        .attr('z-index', 20)
+                                                        .attr('height', cds_height)
+                                                        .attr('width', x(innerChild.fmax) - x(innerChild.fmin));
+                                                }
+                                            });
+                                        }
+                                        else if (isoform_count == MAX_ISOFORMS) {
+                                            ++isoform_count;
+                                            viewer.append('a')
+                                                .attr('class', 'GF transcriptLabel')
+                                                .attr('xlink:href', externalUrl)
+                                                .attr('xlink:show', 'new')
+                                                .append('text')
+                                                .attr('x', x(feature.fmin) + 30)
+                                                .attr('y', isoform_height * isoform_count + isoform_title_height)
+                                                .attr('fill', 'red')
+                                                .attr('opacity', 1)
+                                                .attr('height', isoform_title_height)
+                                                .text('Maximum features displayed.  See full view for more.');
+                                        }
+                                    }
+                                });
+                            }
+
+
+                            if (isoform_count == 0) {
+                                viewer.append('text')
+                                    .attr('x', 30)
+                                    .attr('y', isoform_title_height + 10)
+                                    .attr('fill', 'orange')
+                                    .attr('opacity', 0.6)
+                                    // .attr('height', isoform_title_height)
+                                    .text('Overview of non-coding genome features unavailable at this time.');
+                            }
+                            else {
+                                viewer.append('g')
+                                    .attr('class', 'GF axis')
+                                    .attr('width', width)
+                                    .attr('height', 20)
+                                    .attr('transform', 'translate(0,20)')
+                                    .call(xAxis);
+                            }
 
                         }
                     });
