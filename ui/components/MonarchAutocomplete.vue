8abd419d580dd5fe8cc8abb48bcb6e52c651c952
Tim Putman <tputman@Tims-MacBook-Pro.local>
2018-04-17 10:44:00 -0700

    fixed indention level issue

--- ui/components/MonarchAutocomplete.vue
+++ ui/components/MonarchAutocomplete.vue
@@ -82,152 +82,149 @@
   </div>
 </template>
 
-<script type="text/babel">
-  import * as MA from '../../js/MonarchAccess';
-
-  const debounce = require('lodash/debounce');
-
-  export default {
-    name: 'AutoComplete',
-    props: {
-      homeSearch: {
-        type: String,
-        required: true,
-        default: false,
-      },
+<script type="text/babel">import * as MA from '../../js/MonarchAccess';
+const debounce = require('lodash/debounce');
+export default {
+  name: 'AutoComplete',
+  props: {
+    homeSearch: {
+      type: String,
+      required: true,
+      default: false,
     },
-    data() {
-      return {
-        selected: [],
-        options: [
-          {text: 'Gene', value: 'gene'},
-          {text: 'Genotype', value: 'genotype'},
-          {text: 'Variant', value: 'variant locus'},
-          {text: 'Phenotype', value: 'phenotype'},
-          {text: 'Disease', value: 'disease'},
-        ],
-        catDropDown: false,
-        value: '',
-        suggestions: [],
-        open: false,
-        current: 0,
-        loading: false,
-      };
+  },
+  data() {
+    return {
+      selected: [],
+      options: [
+        {text: 'Gene', value: 'gene'},
+        {text: 'Genotype', value: 'genotype'},
+        {text: 'Variant', value: 'variant locus'},
+        {text: 'Phenotype', value: 'phenotype'},
+        {text: 'Disease', value: 'disease'},
+      ],
+      catDropDown: false,
+      value: '',
+      suggestions: [],
+      open: false,
+      current: 0,
+      loading: false,
+    };
+  },
+  filters: {
+    allLower(word) {
+      return word.toLowerCase();
     },
-    filters: {
-      allLower(word) {
-        return word.toLowerCase();
-      },
-    },
-    methods: {
-      debounceInput: debounce(
-        // eslint-disable-next-line
-        function () {
-          this.fetchData();
-        }, 500, {leading: false, trailing: true}),
-      async fetchData() {
-        const that = this;
-        try {
-          let searchResponse = await MA.getSearchTermSuggestions(this.value, this.selected);
-          searchResponse.docs.forEach(elem => {
-            const resultPacket = {
-              match: elem.match,
-              category: that.categoryMap(elem.category),
-              taxon: that.checkTaxon(elem.taxon_label),
-              curie: elem.id,
-              highlight: elem.highlight,
-              has_hl: elem.has_highlight,
-            };
-            this.suggestions.push(resultPacket);
-          });
-          this.open = true;
-          this.loading = false;
-        }
-        catch (e) {
-          console.log('nodeResponse ERROR', e, that);
-        }
-      },
-      enter() {
-        const currentData = this.suggestions[this.current];
-        // this.$emit('value', { value: this.suggestions[this.current] });
-        this.$router.push({path: `/${currentData.category}/${currentData.curie}`});
-        this.value = '';
-        this.open = false;
-        this.suggestions = [];
-      },
-      // When up pressed while suggestions are open
-      up() {
-        if (this.current > 0) {
-          this.current -= 1;
-        }
-      },
-      // When down pressed while suggestions are open
-      down() {
-        if (this.current < this.suggestions.length - 1) {
-          this.current += 1;
-        }
-      },
-      // For highlighting element
-      isActive(index) {
-        return index === this.current;
-      },
-      mouseOver(index) {
-        this.current = index;
-      },
-      // When one of the suggestion is clicked
-      suggestionClick(index) {
-        const currentData = this.suggestions[index];
-        this.$router.push({path: `/${currentData.category}/${currentData.curie}`});
-        // this.$emit('value', { value: this.suggestions[index] });
-        this.value = '';
-        this.open = false;
-        this.suggestions = [];
-      },
-      showMore() {
-        window.location = `/search/${this.value}`;
-      },
-      clearSearch() {
-        this.suggestions = [];
-        this.value = '';
-      },
-      categoryMap(catList) {
-        const validCats = {
-          'gene': 'gene',
-          'variant locus': 'variant',
-          'phenotype': 'phenotype',
-          'genotype': 'genotype',
-          'disease': 'disease'
-        };
-        const categoryObj = catList.reduce((map, cat) => {
-          cat = validCats[cat];
-          if (cat) {
-            map[cat] = cat;
-          }
-          return map;
-        }, {});
-        return categoryObj.gene ||
-          categoryObj.variant ||
-          Object.keys(categoryObj).join(',');
-      },
-      checkTaxon(taxon) {
-        if (typeof taxon === 'string') {
-          return taxon;
-        }
-      },
-    },
-    watch: {
-      value: function () {
-        this.suggestions = [];
-        if (!this.value) {
-          this.open = false;
-        }
-      },
-      selected: function () {
-        this.suggestions = [];
+  },
+  methods: {
+    debounceInput: debounce(
+      // eslint-disable-next-line
+      function () {
         this.fetchData();
-      },
+      }, 500, {leading: false, trailing: true}),
+    async fetchData() {
+      const that = this;
+      try {
+        let searchResponse = await MA.getSearchTermSuggestions(this.value, this.selected);
+        searchResponse.docs.forEach(elem => {
+          const resultPacket = {
+            match: elem.match,
+            category: that.categoryMap(elem.category),
+            taxon: that.checkTaxon(elem.taxon_label),
+            curie: elem.id,
+            highlight: elem.highlight,
+            has_hl: elem.has_highlight,
+          };
+          this.suggestions.push(resultPacket);
+        });
+        this.open = true;
+        this.loading = false;
+      }
+      catch (e) {
+        console.log('nodeResponse ERROR', e, that);
+      }
     },
-  };
+    enter() {
+      const currentData = this.suggestions[this.current];
+      // this.$emit('value', { value: this.suggestions[this.current] });
+      this.$router.push({path: `/${currentData.category}/${currentData.curie}`});
+      this.value = '';
+      this.open = false;
+      this.suggestions = [];
+    },
+    // When up pressed while suggestions are open
+    up() {
+      if (this.current > 0) {
+        this.current -= 1;
+      }
+    },
+    // When down pressed while suggestions are open
+    down() {
+      if (this.current < this.suggestions.length - 1) {
+        this.current += 1;
+      }
+    },
+    // For highlighting element
+    isActive(index) {
+      return index === this.current;
+    },
+    mouseOver(index) {
+      this.current = index;
+    },
+    // When one of the suggestion is clicked
+    suggestionClick(index) {
+      const currentData = this.suggestions[index];
+      this.$router.push({path: `/${currentData.category}/${currentData.curie}`});
+      // this.$emit('value', { value: this.suggestions[index] });
+      this.value = '';
+      this.open = false;
+      this.suggestions = [];
+    },
+    showMore() {
+      window.location = `/search/${this.value}`;
+    },
+    clearSearch() {
+      this.suggestions = [];
+      this.value = '';
+    },
+    categoryMap(catList) {
+      const validCats = {
+        'gene': 'gene',
+        'variant locus': 'variant',
+        'phenotype': 'phenotype',
+        'genotype': 'genotype',
+        'disease': 'disease'
+      };
+      const categoryObj = catList.reduce((map, cat) => {
+        cat = validCats[cat];
+        if (cat) {
+          map[cat] = cat;
+        }
+        return map;
+      }, {});
+      return categoryObj.gene ||
+        categoryObj.variant ||
+        Object.keys(categoryObj).join(',');
+    },
+    checkTaxon(taxon) {
+      if (typeof taxon === 'string') {
+        return taxon;
+      }
+    },
+  },
+  watch: {
+    value: function () {
+      this.suggestions = [];
+      if (!this.value) {
+        this.open = false;
+      }
+    },
+    selected: function () {
+      this.suggestions = [];
+      this.fetchData();
+    },
+  },
+};
 </script>
 
 <style scoped>
