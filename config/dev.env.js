cdae17df32d48d9edde6c7e66809dccab8932524
DoctorBud <dan@quantumclay.com>
2018-07-16 13:18:14 -0700

    fix config/dev.env.js to point to prod

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,6 @@
 module.exports = {
-  BIOLINK: '"https://api-dev.monarchinitiative.org/api/"',
-  SCIGRAPH_ONTOLOGY: '"https://scigraph-ontology-dev.monarchinitiative.org/"',
-  SCIGRAPH_DATA: '"https://scigraph-data-dev.monarchinitiative.org/"',
-  OWLSIM: '"https://beta.monarchinitiative.org/owlsim"'
+  BIOLINK: '"https://api.monarchinitiative.org/api/"',
+  SCIGRAPH_ONTOLOGY: '"https://scigraph-ontology.monarchinitiative.org/"',
+  SCIGRAPH_DATA: '"https://scigraph-data.monarchinitiative.org/"',
+  OWLSIM: '"https://monarchinitiative.org/owlsim"'
 };
